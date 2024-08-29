34197a04d274fcf75796c6a505c2b9617de1b924
abinash <development.aayanindia@gmail.com>
2024-08-29 18:01:20 +0530

    'updatehome'

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -10,50 +10,69 @@ import stylist from "./img/stylist.png";
 import analysis from "./img/analysis.png";
 
 const Home = () => {
+
   const itemsLeft = [
     { link: "/profile", imgSrc: profile, text: "Profile" },
     { link: "/closet-management", imgSrc: closet, text: "Closet Management" },
     { link: "/stylist", imgSrc: stylist, text: "Stylist" },
-  ];
-
-  const itemsRight = [
     { link: "/market-place", imgSrc: market, text: "Market Place" },
     { link: "/my-style-capsule", imgSrc: style, text: "My Style Capsule" },
-    { link: "/analytics-insights", imgSrc: analysis, text: "Analysis & Insights"},
+    { link: "/analytics-insights", imgSrc: analysis, text: "Analysis & Insights" },
   ];
 
   return (
-    <div className="home">
-      <div className="center">
-        <p>Home</p>
-      </div>
-      <div className="home-row">
-        <div className="left">
-          {itemsLeft.map((item, index) => (
-            <Link key={index} to={item.link} className="text-decoration-none">
-              <div className="closet-management">
-                <div className="image">
-                  <img src={item.imgSrc} alt={`${item.text} Icon`} />
-                </div>
-                <div className="text fw-bold">{item.text}</div>
+    <>
+      <div className="home-container-title">
+        <div className="container text-center">
+          <h4 className="fw-bold text-decoration-underline">Home</h4>
+          <div className="row gx-5">
+            {itemsLeft.map((item, index) => (
+              <div key={index} className="col-12 col-md-6 col-lg-4">
+                <Link to={item.link} className="text-decoration-none">
+                  <div className="closet-management">
+                    <div className="image">
+                      <img src={item.imgSrc} alt={`${item.text} Icon`} className="img-fluid" />
+                    </div>
+                    <div className="home-title-content fw-bold">{item.text}</div>
+                  </div>
+                </Link>
               </div>
-            </Link>
-          ))}
-        </div>
-        <div className="right">
-          {itemsRight.map((item, index) => (
-            <Link key={index} to={item.link} className="text-decoration-none">
-              <div className="closet-management">
-                <div className="image">
-                  <img src={item.imgSrc} alt={`${item.text} Icon`} />
-                </div>
-                <div className="text fw-bold">{item.text}</div>
-              </div>
-            </Link>
-          ))}
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+      {/* <div className="home">
+        <div className="center">
+          <p>Home</p>
+        </div>
+        <div className="home-row">
+          <div className="left">
+            {itemsLeft.map((item, index) => (
+              <Link key={index} to={item.link} className="text-decoration-none">
+                <div className="closet-management">
+                  <div className="image">
+                    <img src={item.imgSrc} alt={`${item.text} Icon`} />
+                  </div>
+                  <div className="text fw-bold">{item.text}</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+          <div className="right">
+            {itemsRight.map((item, index) => (
+              <Link key={index} to={item.link} className="text-decoration-none">
+                <div className="closet-management">
+                  <div className="image">
+                    <img src={item.imgSrc} alt={`${item.text} Icon`} />
+                  </div>
+                  <div className="text fw-bold">{item.text}</div>
+                </div>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div> */}
+    </>
   );
 };
 
