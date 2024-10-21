55ca55e8412d8e480e05191da3d5859fbf88b30f
abinash <development.aayanindia@gmail.com>
2024-10-21 13:49:22 +0530

    'recreateexplore'

--- src/components/closemanagement/ClosetCategories.jsx
+++ src/components/closemanagement/ClosetCategories.jsx
@@ -3,6 +3,14 @@ import notification from "../../assets/closetmanagement/Group 1806.png";
 import closet from "../../assets/closetmanagement/closet.png";
 import coinhand from "../../assets/closetmanagement/coin-hand.png";
 import imagefocus from "../../assets/closetmanagement/image-focus.png";
+import fav1 from "../../assets/closetmanagement/fav1.png";
+import fav2 from "../../assets/closetmanagement/fav2.png";
+import fav3 from "../../assets/closetmanagement/fav3.png";
+import fav4 from "../../assets/closetmanagement/fav4.png";
+import facebook from "../../assets/closetmanagement/facebook.png";
+import instagram from "../../assets/closetmanagement/instagram.png";
+import tiktok from "../../assets/closetmanagement/tiktok.png";
+import twitter from "../../assets/closetmanagement/twiter.png";
 import { Link } from "react-router-dom";
 import '../../styles/closetManagement.scss';
 
@@ -42,6 +50,13 @@ const cardData = [
 ];
 
 const ClosetCategories = () => {
+
+  const allProfileImages1 = [
+    [fav1, fav2],
+    [fav2, fav3, fav3],
+    [fav3, fav4, fav3],
+  ];
+
   return (
     <div className="d-flex justify-content-center align-items-center close-management-container">
       <div className="container w-50">
@@ -61,9 +76,85 @@ const ClosetCategories = () => {
               </Link>
             </div>
           ))}
+          {/* -------------------------------Favorite Designer--------------- */}
+          <div className="mt-4">
+            <h1 className="fw-bold fs-1 mt-2">Favorite Designer</h1>
+            <div className="row g-2 mt-2">
+              {allProfileImages1.map((imageSet, index) => (
+                <div key={index} className="col-12 col-md-4 mb-2 mb-md-0">
+                  {imageSet.map((src, imgIndex) => (
+                    <img
+                      key={imgIndex}
+                      src={src}
+                      alt={`Closet ${imgIndex}`}
+                      className={`img-fluid ${imgIndex > 0 ? "mt-2" : ""}`}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+          {/* -------------------------------Favorite Searches--------------- */}
+          <div className="mt-4">
+            <h1 className="fw-bold fs-1 mt-2">Favorite Designer</h1>
+            <div className="row g-2 mt-2">
+              {allProfileImages1.map((imageSet, index) => (
+                <div key={index} className="col-12 col-md-4 mb-2 mb-md-0">
+                  {imageSet.map((src, imgIndex) => (
+                    <img
+                      key={imgIndex}
+                      src={src}
+                      alt={`Closet ${imgIndex}`}
+                      className={`img-fluid ${imgIndex > 0 ? "mt-2" : ""}`}
+                    />
+                  ))}
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="mt-4">
+            <h4>Credit Cards & Debit Cards</h4>
+            <div className="row">
+              <div className="col-12">
+                <div className="card-section p-3 d-flex flex-column">
+                  {['xxx7676x77897', 'xxx7676x77897', 'xxx7676x77897'].map((card, index) => (
+                    <div key={index} className="card-item d-flex justify-content-between align-items-center mb-2">
+                      <div>{card}</div>
+                      <div>Used on 22/08/21</div>
+                    </div>
+                  ))}
+                </div>
+              </div>
+            </div>
+
+            {/* Social Media Section */}
+            <h4 className="mt-4">Social Media associated</h4>
+            <div className="row social-icons">
+              <div className="col text-center">
+                <a href="https://www.facebook.com/discountDoor/" target="_blank" rel="noopener noreferrer">
+                  <img src={facebook} alt="Facebook" className="icon-img" />
+                </a>
+              </div>
+              <div className="col text-center">
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                  <img src={instagram} alt="Instagram" className="icon-img" />
+                </a>
+              </div>
+              <div className="col text-center">
+                <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+                  <img src={tiktok} alt="TikTok" className="icon-img" />
+                </a>
+              </div>
+              <div className="col text-center">
+                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                  <img src={twitter} alt="Twitter" className="icon-img" />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-    </div>
+    </div >
   );
 };
 
