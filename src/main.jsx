ea9e3f8aeaf5a31e1404787e42d14db8a87ddd4f
motract <abinashaayan>
2025-03-20 15:41:55 +0530

    removecommentfrommain

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
-import { Elements } from "@stripe/react-stripe-js";  // Import Elements provider
-import { loadStripe } from "@stripe/stripe-js";   // Import loadStripe
+import { Elements } from "@stripe/react-stripe-js";  
+import { loadStripe } from "@stripe/stripe-js";   
 
 import App from "./App.jsx";
 import "./index.css";
 import { store } from "../src/reduxToolkit/store.js";
 
-// Load Stripe outside the component for better performance
 const stripePromise = loadStripe("pk_test_51PqTR903ec58RCFWng6UUUnIZ8R0PmQZL1xVE5Wv6jUIAiS9dxzWobfK6oysU86LJmkvd9I2Adcbbv0jYqLkNcAp00hFGx4UKj");
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <Elements stripe={stripePromise}>   {/* Wrap App inside Elements provider */}
+      <Elements stripe={stripePromise}>   
         <App />
       </Elements>
     </Provider>
