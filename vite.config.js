a312adfe0fc6682732cbe3cb63dc2115166e6d7b
motract <abinashaayan>
2025-05-01 21:33:31 +0530

    updateviteconfig

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// Define the config for Vite
 export default defineConfig({
   plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
+  server: {
+    https: true,  // Enable HTTPS
+  },
 });
